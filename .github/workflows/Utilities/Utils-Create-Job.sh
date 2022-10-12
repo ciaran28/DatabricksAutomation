e922a8bc9c80e54c01cab5b172c371950bc6e555
ciaran28 <ciaranh@microsoft.com>
2022-10-12 09:41:44 +0100

    Creating Databricks Job Functionality

--- .github/workflows/Utilities/Utils-Create-Job.sh
+++ .github/workflows/Utilities/Utils-Create-Job.sh
@@ -67,9 +67,8 @@ for row in $(echo "${JSON}" | jq -r '.Jobs[] | @base64'); do
                                 "notebook_path": "/Repos/abcfe557-fd08-4f66-8887-66d6953aa939/DevelopmentFolder/src/tutorial/scripts/framework_testing/remote_analysis", 
                                 "source": $source 
                                 }
-                        } 
-                    ] 
-                }' )
+                        } ] }' )
+                
     
     echo $JSON_STRING
 
